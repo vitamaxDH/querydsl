44d616c4ad25ac1f22c1e34407b0047bdc6b0d87
Daehan Choi <hellonewtry@gmail.com>
2021-07-16 00:55:28 +0900

    상수 ë문자 더하기

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -3,6 +3,7 @@ package study.querydsl.entity;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -474,6 +475,31 @@ class MemberTest {
 
         for (String s : 기타) {
             System.out.println("s =  " + s);
+        }
+
+    }
+
+    @Test
+    public void constant () throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat () throws Exception {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("String = " + s);
         }
 
     }
